95592cb2bcb2f6b0bd606a99bf8169903fa99e5b
Gard.Kalland <github.blunderer148@passmail.net>
2025-01-01 13:24:41 +0100

    Initial Commit

---
+++ TODO/TODOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOApp.swift
+//  TODO
+//
+//  Created by Gard Heine Kalland on 01/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct TODOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TODOTests/TODOTests.swift
@@ -0,0 +1,17 @@
+//
+//  TODOTests.swift
+//  TODOTests
+//
+//  Created by Gard Heine Kalland on 01/01/2025.
+//
+
+import Testing
+@testable import TODO
+
+struct TODOTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
